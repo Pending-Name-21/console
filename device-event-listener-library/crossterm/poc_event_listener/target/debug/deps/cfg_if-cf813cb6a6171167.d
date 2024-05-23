46d7c34b449ab54764ac40da7f467fed77fa98a5
Misax148 <axel.ayala.9412013@usalesiana.edu.bo>
2024-05-23 17:35:01 -0400

    feat: crossterm colors example

---
+++ device-event-listener-library/crossterm/poc_event_listener/target/debug/deps/cfg_if-cf813cb6a6171167.d
@@ -0,0 +1,5 @@
+/home/axel/Documents/JALA-USB/Desarrollo_Software_5/proyects_rust/repo_proyect_console_v2/console/device-event-listener-library/crossterm/poc_event_listener/target/debug/deps/libcfg_if-cf813cb6a6171167.rmeta: /home/axel/.cargo/registry/src/index.crates.io-6f17d22bba15001f/cfg-if-1.0.0/src/lib.rs
+
+/home/axel/Documents/JALA-USB/Desarrollo_Software_5/proyects_rust/repo_proyect_console_v2/console/device-event-listener-library/crossterm/poc_event_listener/target/debug/deps/cfg_if-cf813cb6a6171167.d: /home/axel/.cargo/registry/src/index.crates.io-6f17d22bba15001f/cfg-if-1.0.0/src/lib.rs
+
+/home/axel/.cargo/registry/src/index.crates.io-6f17d22bba15001f/cfg-if-1.0.0/src/lib.rs:
---
+++ device-event-listener-library/crossterm/poc_event_listener/target/debug/deps/colors-1a905c2694b9602d.d
@@ -0,0 +1,5 @@
+/home/axel/Documents/JALA-USB/Desarrollo_Software_5/proyects_rust/repo_proyect_console_v2/console/device-event-listener-library/crossterm/poc_event_listener/target/debug/deps/colors-1a905c2694b9602d: src/examples/colors.rs
+
+/home/axel/Documents/JALA-USB/Desarrollo_Software_5/proyects_rust/repo_proyect_console_v2/console/device-event-listener-library/crossterm/poc_event_listener/target/debug/deps/colors-1a905c2694b9602d.d: src/examples/colors.rs
+
+src/examples/colors.rs:
