3b392939e3ade1ccf88e7927df68100709dc3749
luiggy2620 <luiggymamani@gmail.com>
2024-05-23 20:49:44 -0400

    ci: simulate movement in place form game

--- shared-memory/SharedMemory/src/Main.java
+++ shared-memory/SharedMemory/src/Main.java
@@ -4,16 +4,19 @@ import java.nio.MappedByteBuffer;
 import java.nio.channels.FileChannel;
 
 public class Main {
-    private static final String MMAP_PATH = "/home/fundacion/University/Fifth/SoftwareDevelopment/SharedMemory/text.txt";
+
+    private static final String MMAP_PATH = "/home/fundacion/University/Fifth/SoftwareDevelopment/console/shared-memory/SharedMemory/text.txt";
     private static final int ROWS = 5;
     private static final int COLS = 5;
 
     static {
-        System.load(new File("/home/fundacion/University/Fifth/SoftwareDevelopment/SharedMemory/libshlib.so")
+        System.load(new File("/home/fundacion/University/Fifth/SoftwareDevelopment/console/shared-memory/SharedMemory/libshlib.so")
                 .getAbsolutePath());
     }
 
     public static void main(String[] args) throws Exception {
+        Main mainTwo = new Main();
+
         try (RandomAccessFile sc = new RandomAccessFile(MMAP_PATH, "rw")) {
             MappedByteBuffer buffer = sc.getChannel().map(FileChannel.MapMode.READ_WRITE, 0, ROWS * COLS);
 
@@ -30,47 +33,41 @@ public class Main {
                     buffer.put((byte) c);
                 }
             }
+        }
 
-            int x = 0;
-            int y = 0;
-            int rowToModify = 1;
-            int colToModify = 0;
-            char cursorChar = 'x';
-            char pointChar = '.';
-            int oldIndex, newIndex;
+        moveAndPrintMatrix(ROWS, COLS, mainTwo);
+    }
 
-            oldIndex = x * COLS + y;
-                buffer.put(oldIndex, (byte) pointChar);
-                newIndex = rowToModify * COLS + colToModify;
-                buffer.put(newIndex, (byte) cursorChar);
-                y = rowToModify;
-                rowToModify++;
+    private static void moveAndPrintMatrix(int rows, int cols, Main mainTwo) throws Exception {
+        try (RandomAccessFile sc = new RandomAccessFile(MMAP_PATH, "rw")) {
+            MappedByteBuffer buffer = sc.getChannel().map(FileChannel.MapMode.READ_WRITE, 0, rows * cols);
 
-/*             for (int i = 0; i < 5; i++) {
-                oldIndex = x * COLS + y;
-                buffer.put(oldIndex, (byte) pointChar);
-                newIndex = rowToModify * COLS + colToModify;
-                buffer.put(newIndex, (byte) cursorChar);
-                y = rowToModify;
-                rowToModify++;
-                main.notifyMove(ROWS, COLS);
-            } */
+            int x = 0, y = 0;
 
-            /* rowToModify--;
-            colToModify++;
+            int[][] movements = {
+                    {0, 1},  // right
+                    {1, 0},  // bottom
+                    {0, -1}, // left
+                    {-1, 0}  // top
+            };
 
-            for (int i = 0; i < 5; i++) {
-                oldIndex = x * COLS + y;
-                buffer.put(oldIndex, (byte) pointChar);
-                newIndex = rowToModify * COLS + colToModify;
-                buffer.put(newIndex, (byte) cursorChar);
-                x = colToModify;
-                colToModify++;
-                main.notifyMove(ROWS, COLS);
-            } */
+            for (int[] move : movements) {
+                int newX = x + move[0];
+                int newY = y + move[1];
 
+                if (newX >= 0 && newX < rows && newY >= 0 && newY < cols) {
+                    buffer.put(x * cols + y, (byte) '.');
+                    buffer.put(newX * cols + newY, (byte) 'x');
+
+                    x = newX;
+                    y = newY;
+
+                    mainTwo.notifyMove(rows, cols);
+                    System.out.println();
+                }
+            }
         }
     }
 
-    // public native void notifyMove(int x, int y);
+    public native void notifyMove(int x, int y);
 }
